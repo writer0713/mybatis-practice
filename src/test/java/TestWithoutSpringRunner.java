a6f7e202ad76e708d3a23e0cb02d3e6e56974bdb
KIM JUNGHWAN <writer0713@naver.com>
2018-10-10 16:26:22 +0900

    test 추가
    - ClassPathXmlApplicationContext를 사용하여 간단히 테스트

---
+++ src/test/java/TestWithoutSpringRunner.java
@@ -0,0 +1,29 @@
+import com.writer0713.domains.Db;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+public class TestWithoutSpringRunner {
+
+    private ApplicationContext ac;
+
+    private SimpleDriverDataSource dataSource;
+
+    @Before
+    public void setting() {
+        ac = new ClassPathXmlApplicationContext("spring/mybatis-context.xml");
+    }
+
+    @Test
+    public void testDataSourceIsNotNull() {
+        this.dataSource = this.ac.getBean(SimpleDriverDataSource.class);
+        Assert.assertNotNull(this.dataSource);
+    }
+
+
+
+}
