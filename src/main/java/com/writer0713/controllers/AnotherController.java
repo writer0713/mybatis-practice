075d75ba9ba2117c1a91e5a54b3662c01455c05b
KIM JUNGHWAN <writer0713@naver.com>
2018-10-26 16:35:22 +0900

    @PostConstruct, @PreDestroy 적용
    어떤식으로 적용되는지 테스트

--- src/main/java/com/writer0713/controllers/AnotherController.java
+++ src/main/java/com/writer0713/controllers/AnotherController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -25,6 +27,16 @@ public class AnotherController {
 		String formattedDate = dateFormat.format(date);
 
 		return formattedDate;
+	}
+
+	@PostConstruct
+	private void init() {
+		logger.info("{}", "Another Controller :: init method :: @PostConstruct :: is called");
+	}
+
+	@PreDestroy
+	private void destroy() {
+		logger.info("{}", "Another Controller :: destroy method :: @PreDestroy :: is called");
 	}
 	
 }
