e14e52073fd0852f050511889e92eff1848af270
KIM JUNGHWAN <writer0713@naver.com>
2018-11-05 20:39:29 +0900

    AppConfig (java config) 삭제

--- src/test/java/com/writer0713/TestWithoutSpringRunner.java
+++ src/test/java/com/writer0713/TestWithoutSpringRunner.java
@@ -6,8 +6,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Iterator;
 
 public class TestWithoutSpringRunner {
 
@@ -17,7 +25,7 @@ public class TestWithoutSpringRunner {
 
     @Before
     public void setting() {
-        ac = new ClassPathXmlApplicationContext("spring/mybatis-context.xml");
+        ac = new ClassPathXmlApplicationContext("spring/datasource-context.xml");
     }
 
     @Test
@@ -25,7 +33,4 @@ public class TestWithoutSpringRunner {
         this.dataSource = this.ac.getBean(SimpleDriverDataSource.class);
         Assert.assertNotNull(this.dataSource);
     }
-
-
-
 }
